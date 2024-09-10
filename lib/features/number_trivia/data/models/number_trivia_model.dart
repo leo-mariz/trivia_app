1d9f1673633444739a966aed3d5030cb34c6bdce
leo-mariz <132760586+leo-mariz@users.noreply.github.com>
2024-09-10 17:43:33 -0300

    Adicionado testes de json e dados

---
+++ lib/features/number_trivia/data/models/number_trivia_model.dart
@@ -0,0 +1,22 @@
+import 'package:app/features/number_trivia/domain/entities/number_trivia.dart';
+
+class NumberTriviaModel extends NumberTrivia{
+  const NumberTriviaModel({
+    required super.number,
+    required super.text,
+  });
+
+  factory NumberTriviaModel.fromJson(Map<String, dynamic> json) {
+    return NumberTriviaModel(
+      number: (json['number'] as num).toInt(),
+      text: json['text'],
+    );
+  }
+
+  Map<String, dynamic> toJson() {
+    return {
+      'number': number,
+      'text': text,
+    };
+  }
+}
